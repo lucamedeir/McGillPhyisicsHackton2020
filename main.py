b5a5f0ada0d7bdfc346fde5edf888a752d791bbc
Lucas Medeiros <lucamedeir@gmail.com>
2020-11-07 18:44:57 -0300

    removed window.py, now everything is on main.py

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ from window import *
 from physics import *
 
 def main(argv):
-    print(argv)
-    data,size = our_function()
-    print(size)
-    run_window("McGill Physics Hackaton 2020",data,size)
+    pygame.init()
+
+    size = width, height = 640, 480
+
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.set_caption("McGill Physics Hackaton 2020")
+
+
+
+    while 1:
+        data,size = our_function()
+        plot = pygame.image.fromstring(data, size, "RGB")
+        plotrect = plot.get_rect()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        screen.blit(plot, plotrect)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main(sys.argv)
