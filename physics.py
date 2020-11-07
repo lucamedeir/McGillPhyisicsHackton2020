47b49367758c880550e00cb0db33659ae683f106
Lucas Medeiros <lucamedeir@gmail.com>
2020-11-07 19:35:02 -0300

    corrected function call

--- physics.py
+++ physics.py
@@ -13,11 +13,11 @@ def plot_data(fig,ax,X,Y):
     ax.plot(X, Y)
     return _fig2rgb_array(fig)
 
-def process_data(t,w):
+def process_data(t,w,A):
     X = np.linspace(0, 40*np.pi, 1000)
     phase = X+t*w
 
-    Y = (1/t) * np.exp((-(X-w*t)**2)/(10*t**2))
+    Y = A*(1/t) * np.exp((-(X-w*t)**2)/(10*t**2))
 
     #Y = np.cos(phase)
 
