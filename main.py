a1d7557773e9d8287aa3dcbf6ae03e85c85034d8
Lucas Medeiros <lucamedeir@gmail.com>
2020-11-07 22:59:19 -0300

    free package weird solution

--- main.py
+++ main.py
@@ -9,22 +9,30 @@ import numpy as np
 def main(argv):
     pygame,font,fig,ax = create_window("McGill Physics Hackaton 2020",1240,480)
 
-    t = 0.001
-    dt = 0.001
+    t = 0
+    dt = 0.01
     A = 2
     w = 2*np.pi*20
+    N = 1000
 
     textlist = [Label(font,'t',round(t,3),0,0),
                 Label(font,'A',round(A),700,0),
                 Label(font,'w',round(w,2),700,20),
-                Label(font,'dt',dt,700,40)]
+                Label(font,'dt',dt,700,40),
+                Label(font,'N',N,700,60)]
 
+
+    Y = np.zeros(N)
+    dx = 40*np.pi/N
     while 1:
 
         textlist[0].update_value(round(t,3))
-        X,Y = process_data(t,w,A)
+        X,Y = process_data_real(t,dt,Y)
         t += dt
-        plot,size = plot_data(fig,ax,X,Y)
+        Amp = np.real(np.conj(Y)*Y)
+        Y = Y/np.sqrt(np.sum(Amp*dx))
+        Amp = Amp/np.sum(Amp*dx)
+        plot,size = plot_data(fig,ax,X,Amp)
 
         handle_events(pygame,textlist)
         render(pygame,textlist,plot,size)
--- physics.py
+++ physics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as sint
 
 def _fig2rgb_array(fig):
     '''Convert plot in figure to a string buffer'''
@@ -11,10 +12,30 @@ def _fig2rgb_array(fig):
 def plot_data(fig,ax,X,Y):
     '''Convert data to a string buffer'''
     ax.plot(X, Y)
-    ax.set_ylim(0,20)
     return _fig2rgb_array(fig)
 
+def psi0(X):
+    return np.exp(-np.power(X-20*np.pi,2)/10)*np.exp(1j)
+
+def process_data_real(t,dt,N,psi):
+    X = np.linspace(0,40*np.pi,N)
+    dx = 40*np.pi/N
+
+    if t == 0:
+        Y = psi0(X)
+    else:
+        Y = psi
+
+    Laplaciano = np.zeros(N,dtype=np.complex)
+    Laplaciano = np.convolve(psi,np.array([1,-2,1]) /dx**2)
+
+
+    Y = Y+1j*Laplaciano[1:N+1]/2*dt
+    return X,Y
+
 def process_data(t,w,A):
+
+
     X = np.linspace(0, 40*np.pi, 1000)
     phase = X+t*w
 
